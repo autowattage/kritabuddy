373b9b00f34fa8412a26050d3921d970aa6ea37f
autowattage <pipandw@gmail.com>
2026-08-14 17:02:38 -0400

    base

--- kritabuddy/kritabuddy.py
+++ kritabuddy/kritabuddy.py
@@ -2,19 +2,16 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from krita import *
 
-class krita_wakatime(Extension):
+class kritabuddy(Extension):
     def __init__(self, parent):
         super().__init__(parent)
-
-        settings_layout = QVBoxLayout()
-
-        self.settings = QDialog()
-        self.settings.setWindowTitle("krita-buddy")
-        self.settings.setLayout(settings_layout)
-        
+      
     def setup(self):
         pass
 
+    def open_window(self):
+        pass
+    
     def createActions(self, window):
-        action = window.createAction("", "krita-buddy")
-        action.triggered.connect(self.open_settings)
+        action = window.createAction("", "kritabuddy")
+        action.triggered.connect(self.open_window)

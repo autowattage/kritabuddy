fc4450179a66d0237f910b7a3187fd5c59d5a86c
autowattage <pipandw@gmail.com>
2026-09-03 16:58:42 -0400

    attempting to randomly animate the widget, to no avail

--- kritabuddy/scripter.py
+++ kritabuddy/scripter.py
@@ -3,7 +3,10 @@ from PyQt6.QtGui import *
 from PyQt6.QtCore import *
 from krita import *
 from time import sleep
+import math
+import random
 
+speed = 3
 h = 150
 w = 85
 class root(QWidget):
@@ -13,7 +16,10 @@ class root(QWidget):
         #self.img = QPixmap("/home/bunnyguy/.var/app/org.kde.krita/data/krita/pykrita/kritabuddy/img/idle.png").scaledToHeight(200)
         self.img = QMovie("/home/bunnyguy/.var/app/org.kde.krita/data/krita/pykrita/kritabuddy/img/idle.gif")
         self.img.setScaledSize(QSize(w,h))
-        self.img.start()        
+        self.img.start()
+        self.walkimg = QMovie("/home/bunnyguy/.var/app/org.kde.krita/data/krita/pykrita/kritabuddy/img/walk.gif")
+        self.walkimg.setScaledSize(QSize(w,h))
+        self.walkimg.start()
         self.sprite = QLabel()
         self.sprite.setMovie(self.img)
         self.sprite.mousePressEvent = self.clicked
@@ -24,20 +30,31 @@ class root(QWidget):
         self.layout.addWidget(self.sprite)
         self.setLayout(self.layout)
         self.setFixedSize(w,h)#self.img.width(),self.img.height())
+        self.move(0,parent.height()-h)
         
-        self.move(parent.width()-w,parent.height()-h)
-        # animation
-        self.localmove(0,parent.height()-h,
-                       parent.width()-w,parent.height()-h,30000)
-        self.pos.start()
     def clicked(self, event):
         self.hide()
         
-    def localmove(self, x,y,dx,dy,dt):
-        self.pos = QPropertyAnimation(self, b"pos", self)
-        self.pos.setDuration(dt)
-        self.pos.setStartValue(QPoint(x, y))
-        self.pos.setEndValue(QPoint(dx, dy))
+    def localmove_end(self):
+        self.sprite.setMovie(self.img)
+        
+    def localmove(self,dx,dy,dt):
+        self.tween = QPropertyAnimation(self, b"pos", self)
+        self.tween.finished.connect(self.localmove_end)
+        self.tween.setDuration(dt)
+        self.tween.setStartValue(QPoint(self.pos().x(), self.pos().y()))
+        self.tween.setEndValue(QPoint(dx, dy))
+        self.sprite.setMovie(self.walkimg)
+        self.tween.start()
+        
 
-root(Krita.instance().activeWindow().qwindow().centralWidget()).show()
-#root().show()
+widget = root(Krita.instance().activeWindow().qwindow().centralWidget())
+widget.show()
+def scoot():
+    dx=random.randrange(0,widget.parent().width()-w)
+    dy=random.randrange(0,widget.parent().height()-h)
+    dt=round(math.hypot(dx,dy)/speed*20)
+    widget.localmove(dx,dy,dt)
+    QTimer.singleShot(dt+500, scoot)
+
+scoot()
